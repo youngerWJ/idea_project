3ed129179c25d99bc794feae6632a9dade766f4b
younger <youngerWJ@163.com>
2018-12-28 10:38:34 +0800

    新增get set方法

--- day06_git_demo/src/main/java/com/younger/domain/User.java
+++ day06_git_demo/src/main/java/com/younger/domain/User.java
@@ -6,4 +6,20 @@ package com.younger.domain;
 public class User {
     private String username;
     private Integer age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
